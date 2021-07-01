a9a04381dae8255e53bbde80391605acb90bb31c
Nikita Vorobyev <voronsupermen@gmail.com>
2021-07-01 23:50:32 +0300

    ci next try

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ["__seed__", "dist"],
-  coverageReporters: ['text-summary', 'lcov', 'cobertura']
+  coverageReporters: ['text-summary', 'lcov', 'cobertura', 'jest-junit']
 };

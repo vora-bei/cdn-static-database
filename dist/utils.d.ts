76bcee5417776af3d6632dfd320cf69e7fb2a20c
Nikita Vorobyev <voronsupermen@gmail.com>
2021-07-21 21:00:07 +0300

    ci

--- dist/utils.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function getNext<R extends unknown>(asyncIterator: AsyncIterator<R[], unknown, unknown>, index: number): Promise<{
-    index: number;
-    result: IteratorResult<R[], unknown>;
-}>;
-export declare function combineAsyncIterable(iterable: AsyncIterable<unknown[]>[]): AsyncGenerator<unknown[], unknown[], unknown>;
-export declare function intersectAsyncIterable(iterable: AsyncIterable<unknown[]>[]): AsyncGenerator<unknown[], unknown[], unknown>;
-//# sourceMappingURL=utils.d.ts.map

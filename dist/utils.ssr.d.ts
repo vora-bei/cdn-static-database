8ecc1caff231a32a5fa1d6afc0765b05f27a90db
Nikita Vorobyev <voronsupermen@gmail.com>
2021-07-23 00:54:19 +0300

    files

--- dist/utils.ssr.d.ts
+++
@@ -1,4 +0,0 @@
-import { ISharedIndice, ISpreadIndice } from "./interfaces";
-export declare const saveSharedIndices: <T, P>(indice: ISharedIndice<T, P>, publicPath?: string) => Promise<void>;
-export declare const restoreSharedIndices: <T, P>(id: string, deserializeShared: (data: any, options: any, deserialize: (data: any, options?: any) => ISpreadIndice<T, P>) => ISharedIndice<T, P>, deserialize: (data: any, options?: any) => ISpreadIndice<T, any>) => Promise<ISharedIndice<T, P>>;
-//# sourceMappingURL=utils.ssr.d.ts.map

d3d4f5c103584be16200208e7736060cf656a995
Nikita Vorobyev <voronsupermen@gmail.com>
2021-08-21 20:59:40 +0300

    release candidate

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './simple.indice';
 export * from './range.linear.indice';
 export * from './text.lex.indice';
 export * from './db';
+export * from './schema';
 export { saveSharedIndices, restoreSharedIndices } from './utils.ssr';
 export { restoreSharedIndices as restoreSharedIndicesBrowser } from './utils.browser';
 export { default as log } from './log';

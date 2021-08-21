57e14330c011e689bec916a7126bc2ee07ee3f70
Nikita Vorobyev <voronsupermen@gmail.com>
2021-08-21 17:18:15 +0300

    es2019 target

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ["__seed__", "dist"],
+  modulePathIgnorePatterns: ['__seed__', 'dist'],
   coverageReporters: ['text-summary', 'lcov', 'cobertura'],
   reporters: [
     'default',

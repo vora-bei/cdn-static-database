06ca651d0175dae1dd117bab32917a0529154a27
Nikita Vorobyev <voronsupermen@gmail.com>
2021-08-24 01:03:08 +0300

    Add documentation

--- babel.config.js
+++ babel.config.js
@@ -9,15 +9,14 @@ module.exports = {
         targets: {
           browsers: ['ie >= 11']
         },
-        exclude: ['transform-async-to-generator', 'transform-regenerator', 'transform-spread'],
+        exclude: ['transform-async-to-generator', 'transform-regenerator'],
         modules: false,
-        loose: true,
+        loose: false,
       }
     ]
   ],
   plugins: [
     // don't use `loose` mode here - need to copy symbols when spreading
-    '@babel/proposal-object-rest-spread',
     NODE_ENV === 'test' && '@babel/transform-modules-commonjs'
   ].filter(Boolean)
 }
